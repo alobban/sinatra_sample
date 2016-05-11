fe81edc381a554f36e0855fbfc8c8f66ab7e8860
Andrew Lobban <andrewglobban@gmail.com>
2016-05-11 17:03:08 -0500

    Add specs for basic routes

--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
 
 get '/' do
-  "Hello World!"
+  "This is the Users Index page"
+end
+
+get '/new' do
+  "This is the New User page"
+end
+
+get '/edit' do
+  "This is the Edit User page"
+end
+
+get '/show' do
+  "This is the Show User page"
+end
+
+get '/delete' do
+  "This is the Delete User page"
 end
--- spec/myapp_spec.rb
+++ spec/myapp_spec.rb
@@ -11,8 +11,28 @@ end
 describe 'myapp service' do
   # include Rack::Test::Methods
 
-  it 'loads home page' do
+  it 'loads User Index page' do
     get '/'
+    expect(last_response).to be_ok
+  end
+
+  it 'loads New User page' do
+    get '/new'
+    expect(last_response).to be_ok
+  end
+
+  it 'loads Edit User page' do
+    get '/edit'
+    expect(last_response).to be_ok
+  end
+
+  it 'loads Show User page' do
+    get '/show'
+    expect(last_response).to be_ok
+  end
+
+  it 'loads Delete User page' do
+    get '/delete'
     expect(last_response).to be_ok
   end
 end
