f630f16ad18c7078a32ff62799778bb7b7ddf76e
Andrew Lobban <andrewglobban@gmail.com>
2016-05-12 15:52:55 -0500

    Configure Gulp process and include dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,20 @@ var gulp = require('gulp'),
     rename = require('gulp-rename');
 
 gulp.task('scripts', function () {
-    return gulp.src(['bower_components/angular/angular.js','bower_components/angular-ui-router/release/angular-ui-router.js'])
+    return gulp.src(['bower_components/angular/angular.min.js','bower_components/angular-ui-router/release/angular-ui-router.min.js','bower_components/jquery/dist/jquery.min.js','bower_components/bootstrap/dist/js/bootstrap.min.js'])
         .pipe(concat('dependencies.js'))
         .pipe(gulp.dest('public/js'));
 });
+
+gulp.task('sass-css', function () {
+    return gulp.src('public/scss/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('copy-css', function () {
+    return gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.css')
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('default', ['scripts', 'sass-css', 'copy-css']);
