ddcfe615b6a616df0c9868667137268a8fa4825c
Andrew Lobban <andrewglobban@gmail.com>
2016-05-17 17:04:17 -0500

    Add trigger for Edit User redirect

--- public/js/modules/user/user.controller.js
+++ public/js/modules/user/user.controller.js
@@ -10,10 +10,17 @@
         .controller('AfnUserController', AfnUserController)
     ;
 
-    function AfnUserController($scope, $http) {
+    function AfnUserController($scope, $http, $state, $rootScope) {
         // var vm = this;
         var domain = 'http://localhost:9393';
         var apiUrl = domain+'/user';
+        $scope.edit = edit;
+
+        function edit(user) {
+            console.log(user);
+            $rootScope.user = user;
+            $state.go('editUser', {id: user.id});
+        }
 
         $http({
             mehtod: 'GET',
