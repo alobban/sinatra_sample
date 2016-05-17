0218f65366db45454739f32ed5dc45457ae0c7d3
Andrew Lobban <andrewglobban@gmail.com>
2016-05-17 17:06:27 -0500

    Add Edit User behavior

--- public/js/modules/user/afnEditUser.ctrl.js
+++ public/js/modules/user/afnEditUser.ctrl.js
@@ -10,17 +10,37 @@
         .controller('AfnEditUserCtrl', AfnEditUserCtrl)
     ;
 
-    function AfnEditUserCtrl($scope, $http) {
-        // var vm = this;
+    function AfnEditUserCtrl($http, $rootScope, $state) {
         var domain = 'http://localhost:9393';
-        var apiUrl = domain+'/user/:id';
+        var apiUrl = domain+'/user/';
+        var userJson = null;
+        $rootScope.updateUser = updateUser;
+        
+        // console.log(JSON.stringify($rootScope.user));
+        function mapFields(user) {
+            return {
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "gender": user.gender,
+                "city": user.city,
+                "state": user.state
+            };
+        }
 
-        $http({
-            mehtod: 'PUT',
-            url: apiUrl
-        }).success(function(data) {
-            $scope.users = data;
-            console.log($scope.users);
-        });
+        function updateUser(user) {
+            var userMapped = mapFields(user);
+            userJson = JSON.stringify(userMapped);
+            // userJson = userMapped;
+            console.log(userJson);
+            
+            $http.put(apiUrl+user.id, userJson)
+                .success(function(response) {
+                    console.log(response);
+                    $state.go('users');
+                }).error(function(response) {
+                    console.log(response);
+                })
+            ;
+        }        
     }
 })();
