0d799341aa25a8b00c5ba453c4a33e938a7e3ad8
Andrew Lobban <andrewglobban@gmail.com>
2016-05-20 14:43:45 -0500

    Separate Karma specs from RSpec

---
+++ test/js/modules/user/afnUser.ctrl.spec.js
@@ -0,0 +1,21 @@
+"use strict";
+
+describe('AfnUserController', function() {
+
+    // var $httpBackend;
+    //
+    // console.log('About to load the Module afnUser');
+    //
+    // beforeEach(module("afnUser"));
+    //
+    // beforeEach(function() {
+    //    
+    //     inject(function(_$httpBackend_) {
+    //         $httpBackend = _$httpBackend_;
+    //     });
+    // });
+    //
+    // it('gets a list of users', function() {
+    //     expect(true).to_eq(true);
+    // });
+});
