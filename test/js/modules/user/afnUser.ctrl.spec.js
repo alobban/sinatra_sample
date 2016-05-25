788b330f5c1b0ffa44e043d38edfd2b3e7d02108
Andrew Lobban <andrewglobban@gmail.com>
2016-05-25 17:25:26 -0500

    Establish full code coverage on AfnUserController

--- test/js/modules/user/afnUser.ctrl.spec.js
+++ test/js/modules/user/afnUser.ctrl.spec.js
@@ -24,12 +24,11 @@ describe('AfnUserController', function () {
             "updated_at": "2016-05-17 17:23:30 587000000"
         }
     };
-
+    
     var $httpBackend;
     var $rootScope;
     var $state;
     var $scope;
-    var $controller;
     var AfnUserCtrl;
     var apiUrl = 'http://localhost:9393/user';
 
@@ -43,23 +42,71 @@ describe('AfnUserController', function () {
             $rootScope = $injector.get('$rootScope');
             $scope = $rootScope.$new();
             $state = $injector.get('$state');
-            $controller = $injector.get('$controller')('AfnUserController', {
+            AfnUserCtrl = $injector.get('$controller')('AfnUserController', {
                 $scope: $scope
             });
         });
     });
-    
+
     beforeEach(function () {
 
         $httpBackend
             .whenGET(apiUrl)
             .respond(userData)
         ;
+        spyOn($state, 'go');
+        
+        $httpBackend.flush();
     });
 
     it('returns a list of users', function () {
-        $httpBackend.flush();
-
         expect($scope.users).toEqual(userData);
+    });
+    
+    describe('when create method is called', function () {
+        it('routes to edit page', function () {
+            $scope.create();
+            
+            expect($state.go).toHaveBeenCalled();
+        });
+    });
+    
+    describe('when showUser method is called', function () {
+        var item;
+        beforeEach( function () {
+            item = userData["1"]; 
+        });
+        
+        it('routes to edit page', function () {
+            $scope.show(item);
+            
+            expect($state.go).toHaveBeenCalledWith('viewUser', {id: item.id});
+        });
+    });
+    
+    describe('when edit method is called', function () {
+        var item;
+        beforeEach( function () {
+            item = userData["1"]; 
+        });
+        
+        it('routes to edit page', function () {
+            $scope.edit(item);
+            
+            expect($state.go).toHaveBeenCalledWith('editUser', {id: item.id});
+        });
+    });
+    
+    describe('when deleteUser method is called', function () {
+        var item;
+        beforeEach( function () {
+            item = userData["1"]; 
+        });
+        
+        it('routes to edit page', function () {
+            $scope.delete(item);
+            
+            expect($state.go).toHaveBeenCalledWith('deleteUser', {id: item.id});
+        });
     });
 });
